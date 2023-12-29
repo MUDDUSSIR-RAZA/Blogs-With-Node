00d2f414f5f1a9ebd3917bdaec3628d87cc593c5
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-12-29 06:04:18 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 
 const auth = require("./routes/auth");
 const product = require("./routes/product");
+const blog = require("./routes/blog");
+
 const app = express();
 const { mongoose } = require("./model/connection");
 const { verify } = require("./middleware/auth");
@@ -20,15 +22,16 @@ app.use(cors());
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-app.get("/api/v1/users", (req, res) {
-    const users = [
+app.get("/api/v1/users", (req, res) => {
+  const users = [
     { id: 1, name: "John Doe" },
     { id: 2, name: "Jane Doe" },
-             ];
-    return res.status(200).json({ users }); 
+  ];
+  return res.status(200).json({ users });
 });
 
 app.use("/auth", auth);
+app.use("/blog", blog);
 app.use("/product", verify, product);
 
-app.listen(5000);5
+app.listen(5000);

3513566b1d9fa15816a8a48dc445de7163cd2013
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-12-29 06:04:02 +0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -3,20 +3,28 @@ const { createUser, login } = require("../controllers/auth");
 const router = express.Router();
 
 router.post("/signUp", async (req, res) => {
+  console.log("signup");
   try {
-    const resp = await createUser(req.body.email, req.body.password);
-    res.status(200).send(resp);
+    const resp = await createUser(
+      req.body.firstName,
+      req.body.lastName,
+      req.body.email,
+      req.body.password
+    );
+    res.status(200).json(resp);
   } catch (err) {
-    res.send(err);
+    console.log(err);
+    res.status(400).json(err);
   }
 });
 
 router.post("/login", async (req, res) => {
   try {
     const resp = await login(req.body.email, req.body.password);
-    res.status(200).send(resp);
+    console.log(resp);
+    res.status(200).json(resp);
   } catch (err) {
-    res.send(err);
+    res.status(400).json(err);
   }
 });
 

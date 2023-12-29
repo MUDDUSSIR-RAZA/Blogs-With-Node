48b4e754953111afc3cac0c60eb3c3b507ede5a4
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-12-29 06:03:54 +0500

    Create user.js

---
+++ model/db/user.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  email: {
+    type: String,
+    unique: true,
+    validate: {
+      validator: async function (email) {
+        const existingUser = await this.constructor.findOne({ email });
+        return !existingUser;
+      },
+      message: "Email already exists!",
+    },
+  },
+  password: String,
+  picture: String,
+  blogs: [{ type: Schema.Types.ObjectId, ref: "Blog" }],
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;

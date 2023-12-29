b57bf234f0ffd33f753d4cb3b1d60516c7bafd51
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-12-29 06:03:52 +0500

    Update user.js

--- model/user.js
+++ model/user.js
@@ -1,13 +1,18 @@
-const Users = require("../model/db/users");
+const User = require("../model/db/user");
 
-exports.createUser = async (email, password) => {
+exports.createUser = async (firstName, lastName, email, password) => {
   try {
-    const user = new Users({
-      email: email,
-      password: password,
+    const user = new User({
+      firstName,
+      lastName,
+      email,
+      password,
+      picture:
+        "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?size=626&ext=jpg",
     });
     await user.save();
-    return "User Successfully SigUp!"
+    console.log(user._id);
+    return "User Successfully SigUp!";
   } catch (err) {
     throw err;
   }
@@ -15,7 +20,7 @@ exports.createUser = async (email, password) => {
 
 exports.findUser = async (email) => {
   try {
-    return await Users.findOne({ email });
+    return await User.findOne({ email });
   } catch (err) {
     throw err;
   }

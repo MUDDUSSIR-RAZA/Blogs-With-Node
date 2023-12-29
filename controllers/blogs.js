16c49d0e743dcafb2e02463f3cc5702e2a5715f8
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-12-29 06:04:15 +0500

    Create blogs.js

---
+++ controllers/blogs.js
@@ -0,0 +1,27 @@
+const { createBlog, findBlog, deleteBlog, editBlog } = require("../model/blog");
+const {} = require("../model/user");
+
+exports.createBlog = async (author ,title, description) => {
+  try {
+    const resp = await createBlog(author , title, description);
+    return resp;
+  } catch (err) {
+    throw err;
+  }
+};
+
+exports.deleteBlog = async (id) => {
+  try {
+    return await deleteBlog(id);
+  } catch (err) {
+    throw err;
+  }
+};
+
+exports.editBlog = async (id , title , description) => {
+  try {
+    return await editBlog(id , title , description);
+  } catch (err) {
+    throw err;
+  }
+};

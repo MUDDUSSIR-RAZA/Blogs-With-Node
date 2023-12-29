fdf88f788b130ac01a0c355bdb2c345637e89e1b
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-12-29 09:41:46 +0500

    Update user.js

--- model/db/user.js
+++ model/db/user.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
+  firstName: { type: String, required: [true, "First name is required."] },
+  lastName: { type: String, required:  [true, "Last name is required."] },
   email: {
     type: String,
     unique: true,

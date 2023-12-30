22b103f62aec1096b26b184837fa75f31323cd49
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-12-30 13:59:55 +0500

    Update blog.js

--- model/db/blog.js
+++ model/db/blog.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 const blogSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: "User" },
-  title:  { type: String, required: true },
-  description:  { type: String, required: true },
+  title:  { type: String, required: [true, "Title is required."] },
+  description:  { type: String, required: [true, "Description is required."] },
   date: {
     type: Date,
     default: Date.now,

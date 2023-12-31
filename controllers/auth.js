ac4bd313a00627863fe967417f63819f70128852
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-12-31 20:43:42 +0500

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
-const { createUser, findUser } = require("../model/user");
+const { createUser, findUser, updatePassword, updateName, updatePicture } = require("../model/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const User = require("../model/db/user");
 
 exports.createUser = async (firstName, lastName, email, password) => {
   try {
@@ -27,12 +28,56 @@ exports.login = async (email, password) => {
       throw "Wrong Password!";
     }
 
-    let userId = user._id
+    let userId = user._id;
 
-    let token = jwt.sign({userId , email }, process.env.SECRET_KEY, {
+    let token = jwt.sign({ userId, email }, process.env.SECRET_KEY, {
       expiresIn: "24h",
     });
     return token;
+  } catch (err) {
+    throw err;
+  }
+};
+
+exports.updatePassword = async (email, password , newPassword) => {
+  const user = await findUser(email);
+    if (!user) {
+      throw "Wrong Email!";
+    }
+
+    const result = await bcrypt.compare(password, user.password);
+    if (!result) {
+      throw "Wrong Password!";
+    }
+  try {
+    const hashPass = await bcrypt.hash(newPassword, 12);
+    return await updatePassword(email, hashPass);
+  } catch (err) {
+    throw err;
+  }
+};
+
+exports.updateName = async (email, name) => {
+  const user = await findUser(email);
+    if (!user) {
+      throw "Wrong Email!";
+    }
+    
+  try {
+    return await updateName(email, name);
+  } catch (err) {
+    throw err;
+  }
+};
+
+exports.updatePicture = async (email, picture) => {
+  const user = await findUser(email);
+    if (!user) {
+      throw "Wrong Email!";
+    }
+    
+  try {
+    return await updatePicture(email, picture);
   } catch (err) {
     throw err;
   }

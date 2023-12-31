5e3c0ad18d074e81eea9eee1e7a72a33f552069d
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-12-31 20:43:36 +0500

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 
 exports.verify = (req, res, next) => {
   const token = req.headers.cookie;
-
   jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
     if (err) {
       res.status(400).json("un Authorized");
